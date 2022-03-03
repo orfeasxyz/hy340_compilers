37719c11004c69395b8a8daf533f69f9d955cc09
Orfeas Chatzipanagiotis <porfeasx@ics.forth.gr>
2022-03-03 16:11:41 +0200

    Wrote definitions for alpha_token_t member types. Also wrote incomplete version of the token processing function

---
+++ lex.h
@@ -0,0 +1,107 @@
+#ifndef LEX_H
+#define LEX_H
+
+// define x-macros for all enum types
+#define KEYWORD_TYPES \
+	X(IF) \
+	X(ELSE) \
+	X(WHILE) \
+	X(FOR) \
+	X(FUNCTION) \
+	X(RETURN) \
+	X(BREAK) \
+	X(CONTINUE) \
+	X(AND) \
+	X(NOT) \
+	X(OR) \
+	X(LOCAL) \
+	X(TRUE) \
+	X(FALSE) \
+	X(NIL)
+
+#define OPERATOR_TYPES \
+	X(ASSIGN) \
+	X(PLUS) \
+	X(MINUS) \
+	X(MUL) \
+	X(DIV) \
+	X(MOD) \
+	X(EQUAL) \
+	X(NEQUAL) \
+	X(INC) \
+	X(DEC) \
+	X(GT) \
+	X(LT) \
+	X(GET) \
+	X(LET)
+
+#define PUNCT_TYPES \
+	X(CURLY_OPEN) \
+	X(CURLY_CLOSED) \
+	X(SQUARE_OPEN) \
+	X(SQUARE_CLOSED) \
+	X(PAR_OPEN) \
+	X(PAR_CLOSED) \
+	X(SEMI_COLON) \
+	X(COMMA) \
+	X(DOUBLE_DOTS) \
+	X(QUADRUPLE_DOTS) \
+	X(DOT) \
+	X(RANGE)
+
+#define COMMENT_TYPES \
+	X(BLOCK_COMMENT) \
+	X(LINE_COMMENT)
+
+#define X(val) val,
+// add dummy value to complete the expansion of the macro
+enum operator_type {
+	OPERATOR_TYPES
+	DUMMY_OPERATOR 
+};
+
+enum punct_type {
+	PUNCT_TYPES
+	DUMMY_PUNCT
+};
+
+enum keyword_type {
+	KEYWORD_TYPES
+	DUMMY_KEYWORD
+};
+
+enum comment_type {
+	COMMENT_TYPES
+	DUMMY_COMMENT
+};
+
+enum token_category {
+	INTCONST,
+	REALCONST,
+	IDENTIFIER,
+	OPERATOR,
+	PUNCTUATION,
+	KEYWORD,
+	COMMENT
+};
+
+union token_content {
+	int ival;
+	float fval;
+	char *sval;
+	enum operator_type	oval;
+	enum punct_type		pval;
+	enum keyword_type	kval;
+	enum comment_type	cval;
+};
+
+struct alpha_token_t {
+	int line_no;
+	int no;
+	char *text;
+	enum token_category category;
+	union token_content content;
+	struct alpha_token_t *next;
+};
+
+#endif // LEX_H
